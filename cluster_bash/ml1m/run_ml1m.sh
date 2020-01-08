9819c24bc17b3f0c0012e1b6bd759964270c9fb8
k9luo <lucas.luokai@gmail.com>
2020-01-07 20:08:32 -0500

    split tuning bash

---
+++ cluster_bash/ml1m/run_ml1m.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sbatch --nodes=1 --time=24:00:00 --mem=32G --cpus=4 --gres=gpu:1 ml1m-parameter-tuning-part1.sh
+sbatch --nodes=1 --time=24:00:00 --mem=32G --cpus=4 --gres=gpu:1 ml1m-parameter-tuning-part2.sh
