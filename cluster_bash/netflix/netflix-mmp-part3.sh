19dc8fbd1c8318783d0c18f1b20cbbbb5245e4ae
k9luo <lucas.luokai@gmail.com>
2020-01-09 14:32:57 -0500

    added config and cluster bash for netflix

---
+++ cluster_bash/netflix/netflix-mmp-part3.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+source ~/ENV/bin/activate
+cd ~/MultiModesPreferenceEstimation
+python tune_parameters.py --data-dir data/netflix/ --save-path netflix/mmp-part3.csv --parameters config/netflix/mmp-part3.yml
