dc2e6e024ed61bb4ad13da331698ed289d7a0491
Kai Luo <lucas.luokai@gmail.com>
2020-01-15 00:04:59 -0500

    update config

--- cluster_bash/amazon/digital_music/tuning_digital_music-local-part1.sh
+++ cluster_bash/amazon/digital_music/tuning_digital_music-local-part1.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 cd ~/git/MultiModesPreferenceEstimation
 python tune_parameters.py --data-dir data/amazon/digital_music/ --save-path amazon/digital_music/tuning_general/mmp-part1.csv --parameters config/amazon/digital_music/mmp-part1.yml
-python tune_parameters.py --data-dir data/amazon/digital_music/ --save-path amazon/digital_music/tuning_general/mmp-part2.csv --parameters config/amazon/digital_music/mmp-part2.yml
